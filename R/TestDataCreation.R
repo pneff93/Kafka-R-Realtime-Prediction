be8e10e6882ff6e26b8a49bc92edd739eef031d5
pneff93 <patrick.neff@baader.com>
2021-06-03 15:44:36 +0200

    trigger table working

--- R/TestDataCreation.R
+++ R/TestDataCreation.R
@@ -4,51 +4,42 @@ library(jsonlite)
 library(readr)
 
 
-# Salmon first shift
+# Salmon first shift: 3 min
 set.seed(101)
 
-length <- round(rnorm(200, 88, 5), 3)
-height <- round(rnorm(200, 19, 1), 3)
-error <- round(rnorm(200, 0, 0.1), 3)
+length <- round(rnorm(90, 88, 5), 3)
+height <- round(rnorm(90, 19, 1), 3)
+error <- round(rnorm(90, 0, 0.1), 3)
 weight <- 0.05 * length + 0.01 * height + error
 
-data1 <- data.frame(species = "salmon", id = seq(1, 200), length = length, height = height, weight = weight)
+data1 <- data.frame(species = "salmon", id = seq(1, 90), length = length, height = height, weight = weight)
 
 
-# Walleye second shift
+# Walleye second shift : 4 min
 set.seed(102)
 
-length <- round(rnorm(100, 81, 8), 3)
-height <- round(rnorm(100, 17, 2), 3)
-error <- round(rnorm(100, 0, 0.1), 3)
+length <- round(rnorm(120, 81, 8), 3)
+height <- round(rnorm(120, 17, 2), 3)
+error <- round(rnorm(120, 0, 0.1), 3)
 weight <- 0.02 * length + 0.04 * height + error
 
-data2 <- data.frame(species = "walleye", id = seq(201, 400), length = length, height = height, weight = weight)
+data2 <- data.frame(species = "walleye", id = seq(91, 210), length = length, height = height, weight = weight)
 
 
-# Salmon third shift
+# Salmon third shift : 4 min
 set.seed(103)
 
-length <- round(rnorm(200, 88, 5), 3)
-height <- round(rnorm(200, 19, 1), 3)
-error <- round(rnorm(200, 0, 0.1), 3)
+length <- round(rnorm(120, 88, 5), 3)
+height <- round(rnorm(120, 19, 1), 3)
+error <- round(rnorm(120, 0, 0.1), 3)
 weight <- 0.05 * length + 0.01 * height + error
 
-data3 <- data.frame(species = "salmon", id = seq(401, 600), length = length, height = height, weight = weight)
+data3 <- data.frame(species = "salmon", id = seq(211, 330), length = length, height = height, weight = weight)
 
 
-# Walleye fourth shift
-set.seed(104)
-
-length <- round(rnorm(100, 81, 8), 3)
-height <- round(rnorm(100, 17, 2), 3)
-error <- round(rnorm(100, 0, 0.1), 3)
-weight <- 0.02 * length + 0.04 * height + error
-
-data4 <- data.frame(species = "walleye", id = seq(601, 800), length = length, height = height, weight = weight)
 
 # All test data
-data <- rbind(data1, data2, data3, data4)
+data <- rbind(data1, data2, data3)
 data$id <- as.character(data$id)
 
 
